03983ecc9890e7b3f8f5a2bb994844e7020c0851
Adam Olech <aolech@antmicro.com>
2020-05-15 14:27:57 +0200

    Hide timing

--- app/invocation/dense/dense_invocation_tabs.tsx
+++ app/invocation/dense/dense_invocation_tabs.tsx
@@ -21,7 +21,7 @@ export default class DenseInvocationTabsComponent extends React.Component {
       <a href="#artifacts" className={`tab ${this.props.hash == '#artifacts' && 'selected'}`}>
         ARTIFACTS
       </a>
-      <a href="#timing" className={`tab ${this.props.hash == '#timing' && 'selected'}`}>
+      <a href="#timing" hidden className={`tab ${this.props.hash == '#timing' && 'selected'}`}>
         TIMING
       </a>
       <a href="#raw" className={`tab ${this.props.hash == '#raw' && 'selected'}`}>
